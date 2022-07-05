6934212ced97cd3d4086b647e776a4cf1ae681ba
Diya <diyaramesh@hotmail.com>
2022-07-05 22:04:01 +1000

    Version 2.0

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordLength = 15
 
 let passwordOne = document.getElementById("pwd1")
 let passwordTwo = document.getElementById("pwd2")
+
+let lengthEl = document.getElementById("length-el")
 
 function generatePassword() {
 
@@ -15,11 +16,10 @@ function generatePassword() {
     let length = characters.length
     console.log(length)
     
-    for (let i = 0; i < passwordLength; i++) {
+    for (let i = 0; i < lengthEl.value; i++) {
         let randomIndex1 = Math.floor(Math.random() * length)
         let randomIndex2 = Math.floor(Math.random() * length)
         passwordOne.textContent += characters[randomIndex1]
         passwordTwo.textContent += characters[randomIndex2]
-    }
-    
+    }  
 }
